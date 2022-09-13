9c422b85da63202a7783a11387ea7960da21845d
김경환 <kh.kim9700@gmail.com>
2022-09-13 17:26:08 +0900

    FEAT: Sale > Controller, Service, Response, Request 작성

---
+++ backend/src/main/java/com/sea/domain/sale/dto/SaleDto.java
@@ -0,0 +1,33 @@
+package com.sea.domain.sale.dto;
+
+import java.time.LocalDateTime;
+
+import com.sea.domain.sale.db.entity.Sale;
+
+public class SaleDto {
+	int saleId;
+	String saleContractAddress;
+	int saleYn;
+	String saleCashContractAddress;
+	String saleSellerAddress;
+	String saleBuyerAddress;
+	LocalDateTime saleCreatedAt;
+	LocalDateTime saleCompletedAt;
+	int itemId;
+	int saleStartTime;
+	int saleEndTime;
+
+	public SaleDto(Sale sale) {
+		this.saleId = sale.getSaleId();
+		this.saleContractAddress = sale.getSaleContractAddress();
+		this.saleYn = sale.getSaleYn();
+		this.saleCashContractAddress = sale.getSaleCashContractAddress();
+		this.saleSellerAddress = sale.getSaleSellerAddress();
+		this.saleBuyerAddress = sale.getSaleBuyerAddress();
+		this.saleCreatedAt = sale.getSaleCreatedAt();
+		this.saleCompletedAt = sale.getSaleCompletedAt();
+		this.itemId = sale.getFkItemId().getItemId();
+		this.saleStartTime = sale.getSaleStartTime();
+		this.saleEndTime = sale.getSaleEndTime();
+	}
+}
