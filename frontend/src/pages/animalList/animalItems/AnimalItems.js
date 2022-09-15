6767fbbfd0a084b2b572612a695ca3a7061d4e07
bizyoung93 <bizyoung93@gmail.com>
2022-09-15 11:37:40 +0900

    FIX/AnimalList/pagination 취약일 때 동물들이 표시되지 않는 부분 수정

--- frontend/src/pages/animalList/animalItems/AnimalItems.js
+++ frontend/src/pages/animalList/animalItems/AnimalItems.js
@@ -5,15 +5,10 @@ function AnimalItems(props) {
   const animalList = props.animalList
 
   return (
-    <div>
-      <div className={style.animalItems}>
-        {animalList.map((animal, index) => (
-          <AnimalItem animalItem={animal} key={index} />
-        ))}
-      </div>
-      {!animalList.length ? (
-        <div className={style.announcement}>현재 등록된 동물이 없습니다</div>
-      ) : null}
+    <div className={style.animalItems}>
+      {animalList.map((animal, index) => (
+        <AnimalItem animalItem={animal} key={index} />
+      ))}
     </div>
   )
 }
