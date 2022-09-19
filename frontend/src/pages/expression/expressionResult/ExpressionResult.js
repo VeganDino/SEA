4caa496cdd420ee1229a5faa2d27ea66415fc62b
bizyoung93 <bizyoung93@gmail.com>
2022-09-19 14:34:41 +0900

    FEAT: Expression > 완성

--- frontend/src/pages/expression/expressionResult/ExpressionResult.js
+++ frontend/src/pages/expression/expressionResult/ExpressionResult.js
@@ -1,5 +1,40 @@
+import styles from "./ExpressionResult.module.css"
+
 const ExpressionResult = (props) => {
-  return <div>result</div>
+  const result = props.results
+  console.log(result)
+  const uploadResult = props.uploadResult
+  return (
+    <div className={styles.expressionStart}>
+      <div className={styles.expressionHeader}>
+        <h1>여행의 끝, 그리고 또 다른 시작</h1>
+      </div>
+      <div className={styles.imageDiv}>
+        <img
+          className={styles.image}
+          src={require("resources/img/expression/start.jpg")}
+          alt="사진"
+        />
+      </div>
+      <div className={styles.textDiv}>
+        <div className={styles.text}>
+          당신은 <span className={styles.colorSpan}>{result[0]}</span>에서
+          <span className={styles.colorSpan}>
+            {" " + result[1].slice(0, -1)}
+          </span>
+          는 <span className={styles.colorSpan}>{result[2]}</span>이에요.
+          <br />
+          <br />
+          멸종 위기 동물들은
+          <span className={styles.colorSpanLast}>{" " + result[3]}</span> 세상
+          속에서는 당신과 함께 할 수 있지만
+        </div>
+        <button onClick={uploadResult} className={styles.startButton}>
+          동물들을 찾으러 가기
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default ExpressionResult
