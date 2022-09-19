9cffd848a77a0539a23b412a5b87552ad9da672c
김경환 <kh.kim9700@gmail.com>
2022-09-19 14:03:16 +0900

    AnimalController Changed

--- backend/src/main/java/com/sea/domain/animal/controller/AnimalController.java
+++ backend/src/main/java/com/sea/domain/animal/controller/AnimalController.java
@@ -59,7 +59,7 @@ public class AnimalController {
 	}
 
 	@ApiOperation(value = "동물 이미지 등록")
-	@GetMapping(path = "/image", value = "/image", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {"multipart/form-data"})
+	@PutMapping(path = "/image", value = "/image", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {"multipart/form-data"})
 	@ApiResponses({ @ApiResponse(code = 200, message = "성공", response = BaseResponseBody.class),
 			@ApiResponse(code = 400, message = "실패", response = BaseResponseBody.class), })
 	public ResponseEntity<? extends BaseResponseBody> registerAnimalImage(@RequestPart ImageRegisterPostReq registerInfo, @RequestPart MultipartFile file) {
