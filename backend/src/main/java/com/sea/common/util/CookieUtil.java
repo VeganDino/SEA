e3da2a8967908d80261cc39ec274f398a817c575
김경환 <kh.kim9700@gmail.com>
2022-09-21 17:46:27 +0900

    CookieUtil.java Changed

--- backend/src/main/java/com/sea/common/util/CookieUtil.java
+++ backend/src/main/java/com/sea/common/util/CookieUtil.java
@@ -10,6 +10,7 @@ public class CookieUtil {
 
     public Cookie createCookie(String cookieName, String value){
         Cookie cookie = new Cookie(cookieName,value);
+        cookie.setDomain("j7a506.p.ssafy.io");
         cookie.setHttpOnly(true);
         cookie.setMaxAge(JwtTokenUtil.accessTokenExpiration);
         cookie.setPath("/");
