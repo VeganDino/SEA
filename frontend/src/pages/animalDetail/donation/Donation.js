511740bc2b18527dc4ab5010c89ad7d1d87ae74b
sung <janfar@naver.com>
2022-09-29 11:28:13 +0900

    Feat : Dontaion > minting

--- frontend/src/pages/animalDetail/donation/Donation.js
+++ frontend/src/pages/animalDetail/donation/Donation.js
@@ -14,6 +14,7 @@ import Swal from "sweetalert2"
 import withReactContent from "sweetalert2-react-content"
 import Web3 from "web3"
 import { Link, useNavigate } from "react-router-dom"
+import styles from './Donation.module.css'
 
 const Img = styled('img')({
   margin: 'auto',
@@ -29,11 +30,13 @@ export default function Donation(props) {
   const [animalInfo, setAnimalInfo] = useState({})
   const web3 = new Web3(window.ethereum);
   const [balance, setBalance] = useState();
+  const [myAccount, setMyAccount] = useState();
 
   const getCurrentAccount = async() => {
     try {
       const currentAccounts = await web3.eth.getAccounts();
       // console.log(currentAccounts[0]);
+      setMyAccount(currentAccounts[0]);
       const bal = await web3.eth.getBalance(currentAccounts[0]);
       // console.log(balance);
       setBalance(bal/Math.pow(10,18));
@@ -44,10 +47,6 @@ export default function Donation(props) {
     }
   }
 
-  function enterMinting(e) {
-    navigate("/main/minting", {state : {animalId : e.Id}})
-  }
-
   const infoClick = () => {
     MySwal.fire({
       title: <p>{animalInfo.animalKoreanName}에게 기부하시겠습니까?</p>,
@@ -56,45 +55,107 @@ export default function Donation(props) {
             confirmButtonText: "OK",
             cancelButtonText: "Cancel",
             icon: 'warning',
-            timer: 1500
     }).then((result) => {  
       // donation : 내가 기부할 기부금 
       // balance : 잔고
       if (result.isConfirmed) {
         if(donation==null){
-          Swal.fire('미입력', '토큰을 입력해주세요.<br />0.01 SSF 이상 기부 가능합니다.', 'error');
+          Swal.fire('미입력', '토큰을 입력해주세요.<br />0.01 SSF 이상 기부 가능합니다.', 'error')
         }
         else if(balance<0.01){
           Swal.fire('잔액 미달', '토큰잔액이 최소 기부금 미달입니다.<br />충전 후 기부해주세요.', 'error')
         }
-        else if(donation>balance){
-          Swal.fire('잔액 부족', '잔액이 부족합니다. <br />충전 후 기부해주세요', 'error')
-        }
         else if(donation<0.01){
           Swal.fire('최소 기부금 미달', '기부금은 0.01SSF 이상 가능합니다.<br />충전 후 기부해주세요.', 'error')
         }
+        else if(donation>balance){
+          Swal.fire('잔액 부족', '잔액이 부족합니다. <br />충전 후 기부해주세요', 'error')
+        }
         else if((animalInfo.animalMaxItem - animalInfo.animalNowItem)===0){
           Swal.fire('남아있는 NFT 기부 증서가 없습니다.', '기부 증서 없이 기부만 하시겠습니까?.', 'question')
-        }
+            MySwal.fire({
+              title: <p>남아있는 기부증서(NFT)가 없습니다. <br/> 기부만 하시겠습니까?</p>,
+              showConfirmButton: true,
+              showCancelButton: true,
+              confirmButtonText: "OK",
+              cancelButtonText: "Cancel",
+              icon: 'warning',
+            }).then((result) => {
+            if(result.isConfirmed) {
+              console.log("sss");
+              const send = async() => {
+                try {
+                // const myaccount = await web3.eth.getAccounts()
+                // 0.01 자리에 기부금이 들어갑니다
+                const sendEther = donation / 10 ** 18
+                const response = await web3.eth.sendTransaction({
+                  from: myAccount,
+                  to: "0x9Af0415fD6879B159D2F5C59d1f05B66CFF84CAE",
+                  value: sendEther,
+                })
+                    // response가 뜨기 전까지 로딩 돌리기 => response가 도착하면 로딩 끄고 민팅 페이지로 연결
+                    // console.log(response)
+                  } catch (err) {
+                    console.log(err);
+                  }
+                }
+                send()
+
+              Swal.fire(
+                '기부 완료', '기부가 완료되었습니다.', 'success'
+                ).then(() =>
+                navigate("/main/mypage", {
+                  state: {
+                    animalKoreanName: animalInfo.animalKoreanName,
+                    animalEnglishName: animalInfo.animalEnglishName,
+                    animalNowItem: animalInfo.animalNowItem,
+                    animalDesc: animalInfo.animalDesc,
+                  }}))
+            }
+            else Swal.fire('기부 취소', '기부가 취소되었습니다.', 'error')
+            });
+          }
         else {
+          const send = async() => {
+            try {
+            // const myaccount = await web3.eth.getAccounts()
+            // 0.01 자리에 기부금이 들어갑니다
+            const sendEther = parseInt(donation * 10 ** 18)
+            const response = await web3.eth.sendTransaction({
+              from: myAccount,
+              to: "0x9Af0415fD6879B159D2F5C59d1f05B66CFF84CAE",
+              value: sendEther,
+            })
+                // response가 뜨기 전까지 로딩 돌리기 => response가 도착하면 로딩 끄고 민팅 페이지로 연결
+                // console.log(response)
+              } catch (err) {
+                console.log(err);
+              }
+            }
+            send()
           Swal.fire(
             '기부가 완료되었습니다.', '감사합니다. </br>많은 기부 부탁드립니다.</br>Minting 페이지로 넘어갑니다.', 'success'
             ).then(() =>
-            navigate("/main/minting", { state: { animalId: animalId } }));
+            navigate("/main/minting", {
+              state: {
+                animalKoreanName: animalInfo.animalKoreanName,
+                animalEnglishName: animalInfo.animalEnglishName,
+                animalNowItem: animalInfo.animalNowItem,
+                animalDesc: animalInfo.animalDesc,
+              }}))
         }
       } 
-      else
-      Swal.fire('진행 중단', '', 'error')
+      else Swal.fire('진행 중단', '', 'error')
     })
   }
 
   const [donation, setDonation] = useState(null);
   
   const onChangeAccount = (e) => {
-    setDonation({
-      ...donation,
-      [e.target.name]: e.target.value,
-    });
+    setDonation(
+         e.target.value
+    );
+    console.log(donation);
   };   
   
   const navigate = useNavigate();
@@ -112,7 +173,7 @@ export default function Donation(props) {
 
       <Grid container spacing={2} colums={12}>
         <Grid item xs={6}>
-          <CarouselImages animalImgs={animalInfo.animalImg} ></CarouselImages>
+          <CarouselImages animalImgs={animalInfo.animalImg} className={styles.pictureSize}></CarouselImages>
         </Grid>
         <Grid item xs={6} sm container>
           <Grid item xs container direction="column" spacing={2}>
