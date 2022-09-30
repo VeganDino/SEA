1f5ed30379a9bb92f2b6a3a1508ed494c5f6ec68
김경환 <kh.kim9700@gmail.com>
2022-09-30 15:13:49 +0900

    UserTestResult Put

--- backend/src/main/java/com/sea/domain/user/request/UserUpdateTestResultPutReq.java
+++ backend/src/main/java/com/sea/domain/user/request/UserUpdateTestResultPutReq.java
@@ -1,11 +1,19 @@
 package com.sea.domain.user.request;
 
-import java.util.Arrays;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class UserUpdateTestResultPutReq  {
 	String walletAddress;
-    String[] list;
+    List<String> list;
 }
