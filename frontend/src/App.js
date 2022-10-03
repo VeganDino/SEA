c96282c3104a5f45aa3478d3258b428d2ff35171
bizyoung93 <bizyoung93@gmail.com>
2022-10-04 00:38:54 +0900

    FEAT: mainpage design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,27 +30,29 @@ function App() {
         <BrowserRouter>
           <Navbar />
           <article>
-            <div
-              className="mainArticleDiv"
-              // style={{ position: "relative", "z-index": "5" }}
-            >
-              <Routes>
-                <Route path="/" element={header}></Route>
-                <Route path="/login" element={<LoginPage />}></Route>
-                <Route path="/main/*" element={<MainPage />}></Route>
-                <Route
-                  path="main/express/*"
-                  element={<ExpressionPage />}
-                ></Route>
-                <Route path="main/mypage/*" element={<MyPage />}></Route>
-                <Route path="main/animalList" element={<AnimalList />}></Route>
-                <Route path="main/sale" element={<SalePage />}></Route>
-                <Route
-                  path="main/animalDetail"
-                  element={<AnimalDetail />}
-                ></Route>
-                <Route path="main/minting" element={<Minting />}></Route>
-              </Routes>
+            <div className="stars">
+              <div className="mainArticleDiv">
+                <Routes>
+                  <Route path="/" element={header}></Route>
+                  <Route path="/login" element={<LoginPage />}></Route>
+                  <Route path="/main/*" element={<MainPage />}></Route>
+                  <Route
+                    path="main/express/*"
+                    element={<ExpressionPage />}
+                  ></Route>
+                  <Route path="main/mypage/*" element={<MyPage />}></Route>
+                  <Route
+                    path="main/animalList"
+                    element={<AnimalList />}
+                  ></Route>
+                  <Route path="main/sale" element={<SalePage />}></Route>
+                  <Route
+                    path="main/animalDetail"
+                    element={<AnimalDetail />}
+                  ></Route>
+                  <Route path="main/minting" element={<Minting />}></Route>
+                </Routes>
+              </div>
             </div>
           </article>
           <Footer />
--- frontend/src/pages/main/Mainpage.js
+++ frontend/src/pages/main/Mainpage.js
@@ -1,47 +1,83 @@
-import React from 'react';
-import { useEffect, useState } from "react";
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Footer from '../../components/footer/Footer'
-import { Routes, Route } from 'react-router-dom';
-import ExpressionPage from '../expression/ExpressionPage';
-import Navbar from '../../components/navbar/Navbar';
-import MyPage from '../mypage/MyPage';
-import AnimalListPage from '../animalList/AnimalList';
-import AnimalList from '../animalList/AnimalList';
-import Modal from '../../components/modal/Modal'
-import NFTsale from '../sales/nftsale/NFTsale'
-import NFTpurchase from '../sales/nftpurchase/NFTPurchase'
-import NFTTest from './NFTest'
-import cookies from 'react-cookies';
-import { ethers } from "ethers";
-import SweetTest from '../modal/SweetTest'
+import React from "react"
+import { useEffect, useState } from "react"
+import AppBar from "@mui/material/AppBar"
+import Button from "@mui/material/Button"
+import CameraIcon from "@mui/icons-material/PhotoCamera"
+import Card from "@mui/material/Card"
+import CardActions from "@mui/material/CardActions"
+import CardContent from "@mui/material/CardContent"
+import CardMedia from "@mui/material/CardMedia"
+import CssBaseline from "@mui/material/CssBaseline"
+import Grid from "@mui/material/Grid"
+import Stack from "@mui/material/Stack"
+import Box from "@mui/material/Box"
+import Toolbar from "@mui/material/Toolbar"
+import Typography from "@mui/material/Typography"
+import Container from "@mui/material/Container"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+import Footer from "../../components/footer/Footer"
+import { Routes, Route, Navigate } from "react-router-dom"
+import ExpressionPage from "../expression/ExpressionPage"
+import Navbar from "../../components/navbar/Navbar"
+import MyPage from "../mypage/MyPage"
+import AnimalListPage from "../animalList/AnimalList"
+import AnimalList from "../animalList/AnimalList"
+import Modal from "../../components/modal/Modal"
+import NFTsale from "../sales/nftsale/NFTsale"
+import NFTpurchase from "../sales/nftpurchase/NFTPurchase"
+import NFTTest from "./NFTest"
+import cookies from "react-cookies"
+import { ethers } from "ethers"
+import style from "./Mainpage.module.css"
+import { useNavigate, useLocation } from "react-router-dom"
+import api from "../../api/api"
+import Swal from "sweetalert2"
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-const theme = createTheme();
+const theme = createTheme()
 
 export default function Mainpage() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const openModal = () => { setModalOpen(true); };
-  const closeModal = () => { setModalOpen(false); };
+  const { pathname } = useLocation()
 
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  const [modalOpen, setModalOpen] = useState(false)
+  const openModal = () => {
+    setModalOpen(true)
+  }
+  const closeModal = () => {
+    setModalOpen(false)
+  }
 
   // console.log(cookies.load('id'));
+  const navigate = useNavigate()
+  const goToExpression = async () => {
+    const testResult = await api.user.getExpressionsResult()
+    if (!testResult.length) {
+      console.log("업시유")
+      Swal.fire({
+        icon: "info",
+        title: "나 표현하기로 이동!",
+        html: "기부하기 전에<br>자신이 누군지 알아보세요!",
+        confirmButtonColor: "#1787A7",
+        background: "#D3E0EA",
+      }).then(() => {
+        navigate("/main/express")
+      })
+    } else {
+      Swal.fire({
+        icon: "info",
+        title: "동물 목록으로 이동!",
+        text: "이미 자신을 표현하신 적이 있군요",
+        confirmButtonColor: "#1787A7",
+        background: "#D3E0EA",
+      }).then(() => {
+        navigate("/main/animalList")
+      })
+    }
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -56,54 +92,69 @@ export default function Mainpage() {
       </AppBar> */}
       <main>
         {/* Hero unit */}
-        <Box
+        <Container
           sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
+            mt: 15,
+            minHeight: "50rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
           }}
         >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h4"
-              align="center"
-              color="text.primary"
-              gutterBottom>
-              지금 40종의 동물들이 당신의 도움을 기다리고 있습니다.
-            </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              멸종위기종이 많습니다. <br/> 인간도 곧 멸종될 수 있다
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center">
-              <Button variant="contained">기부하러 가기</Button>
-              {/* <Button onClick={buySomething}>구매</Button> */}
-              <button onClick={openModal} >테스트 모달</button>
-              <Modal open={modalOpen} close={closeModal} header="테스트모달"><NFTTest /></Modal>
-              {/* <Modal open={modalOpen} close={closeModal} header="테스트모달"><NFTsale /></Modal> */}
-          
-            </Stack>
-            <SweetTest />
-          </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-          도움을 기다리는 동물들 <br/>
+          {/* 위쪽 이미지 들어갈 곳 */}
+          <div className={style.imageBox}>
+            <img
+              className={style.mainImage}
+              src="/images/logo/main1.png"
+              alt="메인이미지"
+            />
+          </div>
+          {/* 중간 글자 + 이미지 들어갈 Div */}
+          <div className={style.mainMiddle}>
+            <div className={style.mainMiddleColumn}>
+              <img
+                className={style.mainImage}
+                src="/images/logo/main3.png"
+                alt="메인이미지"
+              />
+            </div>
+            <div className={style.mainMiddleColumn}>
+              <div className={style.mainText}>
+                지금 16종의 동물들이 <br />
+                당신의 도움을 기다리고 있습니다
+              </div>
+              <div className={style.mainText}>
+                <button onClick={goToExpression}>도와주기</button>
+              </div>
+            </div>
+            <div className={style.mainMiddleColumn}>
+              <img
+                className={style.mainImage}
+                src="/images/logo/main2.png"
+                alt="메인이미지"
+              />
+            </div>
+          </div>
+          {/* <Button variant="contained">기부하러 가기</Button> */}
+        </Container>
+        {/* <Container sx={{ py: 8 }} maxWidth="md">
+          도움을 기다리는 동물들 <br />
           <Grid container spacing={4}>
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
                 >
                   <CardMedia
                     component="img"
                     sx={{
                       // 16:9
-                      pt: '56.25%',
+                      pt: "56.25%",
                     }}
                     image="https://source.unsplash.com/random"
                     alt="random"
@@ -112,9 +163,7 @@ export default function Mainpage() {
                     <Typography gutterBottom variant="h5" component="h2">
                       동물명 / 위급등급
                     </Typography>
-                    <Typography>
-                      설명 있어도되고 없어도 되고..
-                    </Typography>
+                    <Typography>설명 있어도되고 없어도 되고..</Typography>
                   </CardContent>
                   <CardActions>
                     <Button size="small">상세 페이지/혹은 모달</Button>
@@ -124,13 +173,8 @@ export default function Mainpage() {
               </Grid>
             ))}
           </Grid>
-        </Container>
+        </Container> */}
       </main>
-       {/* <Routes>
-         <Route path="express/*" element={<ExpressionPage />}></Route>
-         <Route path="mypage/*" element={<MyPage />}></Route>
-         <Route path="animalList" element={<AnimalList />}></Route>
-       </Routes> */}
-     </ThemeProvider>
-  );
+    </ThemeProvider>
+  )
 }
