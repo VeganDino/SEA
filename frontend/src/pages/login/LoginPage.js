2a6805f2d83271c0133beaf3e38347295fa2cd32
bizyoung93 <bizyoung93@gmail.com>
2022-10-04 10:47:20 +0900

    FEAT/login/changeWallet

--- frontend/src/pages/login/LoginPage.js
+++ frontend/src/pages/login/LoginPage.js
@@ -1,116 +1,115 @@
-import React, { useEffect, useState } from "react";
-import { Button, Paper, Stack, Typography } from "@mui/material";
-import { useNavigate } from 'react-router-dom';
-import { useCookies } from 'react-cookie'; 
-import { ethers } from "ethers";
-import api from "api/api";
-import coin from '../login/coin.png'
-import loading from '../login/loading.gif'
-import sealogo3 from '../login/sealogo3.png'
-import logo from '../../components/footer/sea.png'
-import './Login.css'
+import React, { useEffect, useState } from "react"
+import { Button, Paper, Stack, Typography } from "@mui/material"
+import { useNavigate } from "react-router-dom"
+import { useCookies } from "react-cookie"
+import { ethers } from "ethers"
+import api from "api/api"
+import coin from "../login/coin.png"
+import loading from "../login/loading.gif"
+import sealogo3 from "../login/sealogo3.png"
+import logo from "../../components/footer/sea.png"
+import "./Login.css"
 import Web3 from "web3"
 
 const LoginPage = () => {
-  const [haveMetamask, sethaveMetamask] = useState(true);
-  const [isConnected, setIsConnected] = useState(false);
+  const [haveMetamask, sethaveMetamask] = useState(true)
+  const [isConnected, setIsConnected] = useState(false)
 
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [account, setAccount] = useState(null);
-  const [balance, setBalance] = useState(null);
-  const [cookies, setCookie] = useCookies();
-  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [account, setAccount] = useState(null)
+  const [balance, setBalance] = useState(null)
+  const [cookies, setCookie] = useCookies()
+  const navigate = useNavigate()
   // const web3 = new Web3(window.web3.currentProvider);
-  const web3 = new Web3(window.ethereum);
+  const web3 = new Web3(window.ethereum)
 
-  const getCurrentAccount = async() => {
+  const getCurrentAccount = async () => {
     try {
-      const currentAccounts = await web3.eth.getAccounts();
-      console.log(currentAccounts[0]);
-      const balance = await web3.eth.getBalance(currentAccounts[0]);
-      console.log(balance);
-      console.log(web3);
-      return currentAccounts[0];
+      const currentAccounts = await web3.eth.getAccounts()
+      console.log(currentAccounts[0])
+      const balance = await web3.eth.getBalance(currentAccounts[0])
+      console.log(balance)
+      console.log(web3)
+      return currentAccounts[0]
     } catch {
-      console.log("err");
+      console.log("err")
     }
   }
 
   useEffect(() => {
-
     if (window.ethereum) {
-      window.ethereum.on("accountsChanged", accountsChanged);
-      window.ethereum.on("chainChanged", chainChanged);
-      getCurrentAccount();
-    };
+      window.ethereum.on("accountsChanged", accountsChanged)
+      window.ethereum.on("chainChanged", chainChanged)
+      getCurrentAccount()
+    }
 
-    const { ethereum } = window;
+    const { ethereum } = window
     const checkMetamaskAvailability = async () => {
       if (!ethereum) {
-        sethaveMetamask(false);
+        sethaveMetamask(false)
       }
-      sethaveMetamask(true);
-    };
-    checkMetamaskAvailability();
-  }, 
-  
-  
-  []);
+      sethaveMetamask(true)
+    }
+    checkMetamaskAvailability()
+  }, [])
 
   const connectHandler = async () => {
     if (window.ethereum) {
       try {
-          const res = await window.ethereum.request({
+        const res = await window.ethereum.request({
           method: "eth_requestAccounts",
-        });
-        await accountsChanged(res[0]);
-        setIsConnected(true);
-        api.user.login(res[0]);
+        })
+        await accountsChanged(res[0])
+        setIsConnected(true)
+        api.user.login(res[0])
       } catch (err) {
-        console.error(err);
-        setErrorMessage("MetaMask 연결 에러");
+        console.error(err)
+        setErrorMessage("MetaMask 연결 에러")
       }
     } else {
       alert("MetaMask를 설치하세요.")
-      setErrorMessage("MetaMask를 설치하세요.");
-      window.open('https://metamask.io/download.html');
+      setErrorMessage("MetaMask를 설치하세요.")
+      window.open("https://metamask.io/download.html")
     }
-  };
+  }
 
   const accountsChanged = async (newAccount) => {
-    setAccount(newAccount);
-    setCookie('id', newAccount);
-    navigate("/main");
+    setAccount(newAccount)
+    setCookie("id", newAccount)
+    navigate("/main")
     try {
       const balance = await window.ethereum.request({
         method: "eth_getBalance",
         params: [newAccount.toString(), "latest"],
-      });
-      setBalance(ethers.utils.formatEther(balance));
+      })
+      setBalance(ethers.utils.formatEther(balance))
     } catch (err) {
-      console.error(err);
-      setErrorMessage("MetaMask 연결 에러");
+      console.error(err)
+      setErrorMessage("MetaMask 연결 에러")
     }
-  };
+  }
 
   const chainChanged = () => {
-    setErrorMessage(null);
-    setAccount(null);
-    setBalance(null);
-  };
-  
+    setErrorMessage(null)
+    setAccount(null)
+    setBalance(null)
+  }
 
   return (
-    <div className='header section__padding'>
-      <img className='shake-vertical' src={sealogo3} width={650} />
+    <div className="header section__padding">
+      <img className="shake-vertical" src={sealogo3} width={650} />
       <div className="header-content">
         <div>
           <h1>
-            우리나라 멸종위기종 NFT 기부 플랫폼<br/><br/> 
-            <h4>멸종위기 동물에게 토큰 기부하고 AI가 그려주는 <br/>
-            동물 NFT 그림 증서 받아가세요!</h4>
+            우리나라 멸종위기종 NFT 기부 플랫폼
+            <br />
+            <br />
+            <h4>
+              멸종위기 동물에게 토큰 기부하고 AI가 그려주는 <br />
+              동물 NFT 그림 증서 받아가세요!
+            </h4>
           </h1>
-          <img className='shake-vertical' src={coin} alt="" />
+          <img className="shake-vertical" src={coin} alt="" />
         </div>
       </div>
 
@@ -118,16 +117,23 @@ const LoginPage = () => {
                 className="App-logo" alt="logo" /> */}
 
       <Stack spacing={2}>
-        <button className='button' onClick={connectHandler}>METAMASK LOGIN</button>
+        <button className="button" onClick={connectHandler}>
+          METAMASK LOGIN
+        </button>
         {errorMessage ? (
           <Typography variant="body1" color="red">
             Error: {errorMessage}
           </Typography>
         ) : null}
       </Stack>
-      <br /><br /><br /><br /><br /><br />
-      </div>
-  );
-};
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  )
+}
 
-export default LoginPage;
+export default LoginPage
