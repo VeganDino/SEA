fc8264858ec3de08448d64b72bf76ff0f03f3cc1
lji <insfamily01@gmail.com>
2022-10-06 22:04:24 +0900

    Feat :LoginPage info alert
    login 화면 인포 화면 생성

--- frontend/src/pages/login/LoginPage.js
+++ frontend/src/pages/login/LoginPage.js
@@ -36,43 +36,39 @@ const LoginPage = () => {
       console.log("err")
     }
   }
-  const showNFTInfo=()=>{
-    
+  const showNFTInfo = () => {
     Swal.fire({
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#3085d6',
-      title: 'METAMASK?',
-      text: "메타마스크가 어떤건가요?",
-      icon: 'question',
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#3085d6",
+      title: "METAMASK?",
+      text: "메타마스크는 가상 화폐 거래를 좀 더 편하게 가능하게 해주는 지갑 역할을 하는 크롬 확장 프로그램입니다!",
+      icon: "question",
       showCancelButton: true,
-      confirmButtonText: 'coin 얻는 방법?',
-      cancelButtonText: 'metamask 사용법',
+      confirmButtonText: "coin을 얻으려면?",
+      cancelButtonText: "metamask 사용법?",
       reverseButtons: true,
-      
     }).then((result) => {
       if (result.isConfirmed) {
-        Swal.fire(
-          {
-            width:1000,
-          title: 'GoerilETH 받기 - 로그인 필요',
+        Swal.fire({
+          width: 1000,
+          title: "GoerilETH 받기 - 로그인 필요",
           html: `
-      1.https://goerlifaucet.com/<br>
-      2.메타마스크에서는 GeorilETH과 연결할 수 있습니다.<br><br>
-      3.네트워크를 Georil 테스트 네트워크로 전환합니다.<br>
-      4.해당 항목이 보이지 않을 시 설정-고급-테스트네트워크 보기 항목을 활성화하여 확인이 가능합니다.<br>
-      5.잔고가 GoerilETH과 연결된 것을 확인한 뒤 위의 링크로 들어갑니다.<br>
-      6.본인의 지갑주소('0x......')를 입력하여 0.1GoerilETH를 지급받을 수 있습니다.<br>`,
-      confirmButtonText: "Next",
-        }
-        ).then(() => {
+      <b>1.<a href="https://goerlifaucet.com/" target="_blank">https://goerlifaucet.com/</a></b><br><br>
+      <b>2.메타마스크에서는 GeorilETH과 연결할 수 있습니다.</b><br><br>
+      <b>3.네트워크를 Georil 테스트 네트워크로 전환합니다.</b><br><br>
+      <b>4.해당 항목이 보이지 않을 시 설정-고급-테스트네트워크 보기 항목을 활성화하여 확인이 가능합니다.</b><br><br>
+      <b>5.잔고가 GoerilETH과 연결된 것을 확인한 뒤 위의 링크로 들어갑니다.</b><br><br>
+      <b>6.본인의 지갑주소('0x......')를 입력하여 0.1GoerilETH를 지급받을 수 있습니다.</b>`,
+          confirmButtonText: "Next",
+        }).then(() => {
           return Swal.fire({
-            width:800,
-          title: 'GoerilETH 받기 - 로그인 불필요',
-          html: `
-          1. https://goerli-faucet.pk910.de/<br>
-          2.해당 링크에 본인의 지갑주소를 입력한 뒤 채굴을 시작할 수 있습니다.<br>
-          3.원하는 시간 동안 채굴하고 보상으로 GoerilETH를 제공받을 수 있습니다.<br>`,
-      confirmButtonText: "Next",
+            width: 800,
+            title: "GoerilETH 받기 - 로그인 불필요",
+            html: `
+            <b>1. <a href="https://goerli-faucet.pk910.de/" target="_blank">https://goerli-faucet.pk910.de/</a></b><br><br>
+          <b>2.해당 링크에 본인의 지갑주소를 입력한 뒤 채굴을 시작할 수 있습니다.</b><br><br>
+          <b>3.원하는 시간 동안 채굴하고 보상으로 GoerilETH를 제공받을 수 있습니다.</b>`,
+            confirmButtonText: "Next",
           })
         })
       } else if (
@@ -80,24 +76,23 @@ const LoginPage = () => {
         result.dismiss === Swal.DismissReason.cancel
       ) {
         Swal.fire({
-          width:800,
-          title: '메타마스크 설치',
+          width: 800,
+          title: "메타마스크 설치",
           html: `
-          1.메타마스크를 설치하기 위해 크롬 웹스토어에 접속합니다.<br><br>
-          2.왼쪽 상단의 스토어 검색에서 Metamask를 검색합니다.<br>
-          3.메타마스크 페이지에서 'Chrome에 추가' 버튼을 눌러주면 설치가 완료됩니다.<br>`,
-      confirmButtonText: "Next",
-        }
-        ).then(() => {
+          <b>1.메타마스크를 설치하기 위해 <a href="https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=ko" target="_blank">크롬 웹스토어</a>에 접속합니다.</b><br><br>
+          <b>2.왼쪽 상단의 스토어 검색에서 Metamask를 검색합니다.</b><br><br>
+          <b>3.메타마스크 페이지에서 'Chrome에 추가' 버튼을 눌러주면 설치가 완료됩니다.</b>`,
+          confirmButtonText: "Next",
+        }).then(() => {
           return Swal.fire({
-            width:800,
-          title: '지갑 생성',
-          html: `
-          1.첫 화면에서 시작하기를 누릅니다.<br>
-          2.다음 화면에서 지갑 생성 버튼을 누릅니다.<br>
-          3.가입 진행 중에 생성되는 비밀 복구 구문을 안전한 장소 및 경로에 보관합니다.<br>
-          4.가입 진행을 마치면 지갑이 생성됩니다.`,
-      confirmButtonText: "Next",
+            width: 800,
+            title: "지갑 생성",
+            html: `
+          <b>1.첫 화면에서 시작하기를 누릅니다.</b><br><br>
+          <b>2.다음 화면에서 지갑 생성 버튼을 누릅니다.</b><br><br>
+          <b>3.가입 진행 중에 생성되는 비밀 복구 구문을 안전한 장소 및 경로에 보관합니다.</b><br><br>
+          <b>4.가입 진행을 마치면 지갑이 생성됩니다.</b>`,
+            confirmButtonText: "Next",
           })
         })
       }
@@ -188,7 +183,6 @@ const LoginPage = () => {
           METAMASK LOGIN
         </button>
 
-       
         {errorMessage ? (
           <Typography variant="body1" color="red">
             Error: {errorMessage}
@@ -196,8 +190,8 @@ const LoginPage = () => {
         ) : null}
       </Stack>
       <button className="infoButton" onClick={showNFTInfo}>
-          METAMASK?
-        </button>
+        METAMASK?
+      </button>
       <br />
       <br />
       <br />
