dfc12f1cc892f8331f6b2985d553c4a0884457a5
lji <insfamily01@gmail.com>
2022-10-07 01:31:15 +0900

    feat: NFTList fix
    NFT 리스트에서 위기 등급 제대로 표시 안되던 것 고침

--- frontend/src/pages/mypage/NFTList/NFTCard.js
+++ frontend/src/pages/mypage/NFTList/NFTCard.js
@@ -9,7 +9,7 @@ export default function NFTCard(props) {
     endangered: props.NFTData.animalEndangeredLevel,
     animalName: props.NFTData.animalKoreanName,
   }
-  //console.log(NFTdata.img) 
+  //console.log(NFTdata.endangered)
 
   const NFTClick = () => {
     Swal.fire({
@@ -70,16 +70,16 @@ export default function NFTCard(props) {
           sx={{ fontSize: 20, fontWeight: "bold", lineHeight: "1.1rem" }}
           component="div"
           style={
-            NFTdata.endangered === "1"
+            NFTdata.endangered === 1
               ? { color: "red" }
-              : NFTdata.endangered === "2"
+              : NFTdata.endangered === 2
               ? { color: "blue" }
               : { color: "green" }
           }
         >
-          {NFTdata.endangered === "1"
+          {NFTdata.endangered === 1
             ? "위급"
-            : NFTdata.endangered === "2"
+            : NFTdata.endangered === 2
             ? "위기"
             : "취약"}
         </Typography>
--- frontend/src/pages/sales/nftsale/NFTCard.js
+++ frontend/src/pages/sales/nftsale/NFTCard.js
@@ -63,7 +63,7 @@ export default function NFTCard(props) {
               fontSize: 20,
               fontWeight: "bold",
               mr: "2.5rem",
-             lineHeight: "0rem"
+              lineHeight: "0rem",
             }}
             component="div"
           >
@@ -73,16 +73,16 @@ export default function NFTCard(props) {
             sx={{ fontSize: 20, fontWeight: "bold", lineHeight: "0rem" }}
             component="div"
             style={
-              NFTdata.endangered === "1"
+              NFTdata.endangered === 1
                 ? { color: "red" }
-                : NFTdata.endangered === "2"
+                : NFTdata.endangered === 2
                 ? { color: "blue" }
                 : { color: "green" }
             }
           >
-            {NFTdata.endangered === "1"
+            {NFTdata.endangered === 1
               ? "위급"
-              : NFTdata.endangered === "2"
+              : NFTdata.endangered === 2
               ? "위기"
               : "취약"}
           </Typography>
